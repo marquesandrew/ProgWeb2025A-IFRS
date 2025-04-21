8a16173e9161bcc397107757ec10c1a2c612eaf4
Andrew Marques <andrewmarques@protonmail.com>
2025-04-21 13:31:32 -0300

    Update script.js

--- 4 JavaScript/aula5-lacos/script.js
+++ 4 JavaScript/aula5-lacos/script.js
@@ -1,6 +1,16 @@
-var repete=1;
+/*var repete=1;
 
 while(repete<4){
     alert("Mensagem"+repete);
     repete++;
 }
+*/
+var inicio=2;
+var fim=10;
+var TodosPares="Números Pares: ";
+
+while (inicio <= 10){
+    TodosPares = TodosPares + inicio+", ";
+    inicio += 2;
+}
+alert(TodosPares);
