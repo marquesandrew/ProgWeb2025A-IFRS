040d5a372d235d1939807e60f8179f7a5fad9700
Andrew Marques <andrewmarques@protonmail.com>
2025-04-21 16:23:36 -0300

    commit

---
+++ 4 JavaScript/aula6-eventos-load/script.js
@@ -0,0 +1,15 @@
+function carreguei() {
+    document.getElementById("log").innerHTML += "<p>Carregou a página</p>";
+}   
+
+function redimensionei() {
+    document.getElementById("log").innerHTML += "<p>Redimensionei a página</p>";
+}
+
+function cliquei() {
+    document.getElementById("log").innerHTML += "<p>Cliquei no botão</p>";
+}
+
+function cliquei2() {
+    document.getElementById("log").innerHTML += "<p>Duplo Cliquei no botão</p>";
+}
